479da17afd10fae884a8e91e61e3ac6a9afa62c7
tamakiii <tamakiii@users.noreply.github.com>
2019-06-04 23:20:57 +0900

    green

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -2,6 +2,7 @@ import org.scalatest._
 
 class HelloSpec extends FunSuite with DiagrammedAssertions {
   test("Hello should start with H") {
-    assert("hello".startsWith("H"))
+    // Hello, as opposed to hello
+    assert("Hello".startsWith("H"))
   }
 }
