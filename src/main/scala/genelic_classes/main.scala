4017226fbe1c725e6119023d19d6f96acd8951cb
tamakiii <tamakiii@users.noreply.github.com>
2019-07-08 23:43:25 +0900

    fruits

--- src/main/scala/genelic_classes/main.scala
+++ src/main/scala/genelic_classes/main.scala
@@ -18,10 +18,33 @@ class Stack[A] {
   }
 }
 
+// class Fruit {
+//   def name: String = ""
+// }
+trait Fruit {
+  def name: String
+}
+class Apple extends Fruit {
+  def name: String = "apple"
+  // override def name: String = "apple"
+}
+class Banana extends Fruit {
+  def name: String = "banana"
+  // override def name: String = "banana"
+}
+
 object Main extends App {
   val stack = new Stack[Int]
   stack.push(1)
   stack.push(2)
   System.out.println(stack.pop)
   System.out.println(stack.pop)
+
+  val fruits = new Stack[Fruit]
+  var apple = new Apple
+  var banana = new Banana
+  fruits.push(new Apple)
+  fruits.push(new Banana)
+  System.out.println(fruits.pop.name)
+  System.out.println(fruits.pop.name)
 }
